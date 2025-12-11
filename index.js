bc8306bc88a1c363541387449ba9d233a509c986
𝗏𝗂𝗇𝗅𝗎𝖺𝗇 𝗃𝖾𝗇𝗇𝗒 𝗆. <153419347+luianvn@users.noreply.github.com>
2025-12-11 20:10:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.use(express.json());
 const PORT = 3000;
 
